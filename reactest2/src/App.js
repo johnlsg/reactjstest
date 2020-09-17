788b3998143ce1ed897c1c1c13e2c9d9203c88f6
John Law <johnlsg9909@gmail.com>
2020-09-17 11:30:41 +0800

    update

--- reactest2/src/App.js
+++ reactest2/src/App.js
@@ -7,51 +7,28 @@ import {
   MOUSE_ACTIVATION,
   TOUCH_ACTIVATION
 } from "react-image-magnifiers";
+import './app.css'
 const image = require('./image.jpg');
 const image2 = require('./image2.PNG');
-const image3= require('./circle.png')
+const imgCircle= require('./circle.png')
+const image3 = require('./image3.jpg')
 
 
 
 
-const ovl = (isZoom) =>{
-
-  const handleClick = (e)=>{
-    e.persist()
-    console.log("clicked")
-    console.log(e)
-  }
-  const handleEnter = (e)=>{
-    e.target.style.opacity="1"
-
-  }
-  const handleExit = (e)=>{
-    e.target.style.opacity="1"
-  }
-  const style = {
-    "position":"absolute",
-    "display":"block",
-    "backgroundColor": "none",
-    "zIndex":3,
-    "left":"260px",
-    "top":"50px",
-    "opacity":"1"
-  }
-  return(
-    <div style={style} onClick={handleClick} onMouseEnter={handleEnter} onMouseLeave={handleExit}>
-      <img src={image3} width="30px"/>
-    </div>
-  )
-}
 
 class App extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {isToggleOn: true};
+    this.state = {
+      marks:[],
+      mx:0,
+      my:0,
+      imgSize:0
+    };
   }
 
-
-  render(){
+  ovl = (isZoom) =>{
     const handleClick = (e)=>{
       console.log("div clicked")
       console.log(e.clientX)
@@ -59,24 +36,102 @@ class App extends React.Component{
       console.log(e.pageX)
       console.log(e.pageY)
       const {offsetTop,offsetLeft} = document.getElementById("bodyMag")
-      console.log(e.pageX-offsetLeft)
-      console.log(e.pageY-offsetTop)
+      let x = e.pageX-offsetLeft
+      let y = e.pageY-offsetTop
+      console.log(x)
+      console.log(y)
+      let marker = this.state.marks.slice()
+      const markId = marker.length
+      marker.push({
+        id:markId,
+        x:x-5-3,
+        y:y-5-3,
+        isHide:false,
+        offsetX:0,
+        offSetY:0
+      })
+      this.setState({marks:marker})
     }
+
+    const handleMove = (e)=>{
+      const {offsetTop,offsetLeft,offsetWidth} = document.getElementById("bodyMag")
+      let cursorX = e.pageX-offsetLeft
+      let cursorY = e.pageY-offsetTop
+      this.setState({mx:cursorX,my:cursorY})
+      const newMarks = this.state.marks.map((val)=>{
+        let dist = Math.pow((val.x+5) - (cursorX-3),2) + Math.pow((val.y+5)-(cursorY-3), 2)
+        const magnifiedRange = (0.25*offsetWidth-6)/this.state.imgSize*offsetWidth
+        if(dist< Math.pow(magnifiedRange/2,2)){
+          const curX = cursorX-3
+          const curY = cursorY-3
+          const markX = val.x+5
+          const markY = val.y+5
+          // const magX = (50/7)*(markX-curX)
+          // const magY = (50/7)*(markY-curY)
+          const magX = (7.016)*(markX-curX)
+          const magY = (7.016)*(markY-curY)
+          const finalX = magX + curX
+          const finalY = magY + curY
+          const diffX = finalX - markX
+          const diffY = finalY - markY
+          return {...val, isHide:false, offsetX: diffX, offsetY: diffY}
+        }else if( dist< Math.pow((0.5*0.25 * offsetWidth)+3,2)){
+          return {...val,isHide:true, offsetX:0, offsetY:0}
+        }
+        return {...val,isHide:false, offsetX:0, offsetY:0}
+      })
+      this.setState({marks:newMarks})
+    }
+    const style = {
+      "position":"absolute",
+      "display":"block",
+      "backgroundColor": "none",
+      "zIndex":3,
+      "left":"0px",
+      "top":"0px",
+      "opacity":"1",
+      "width":"100%",
+      "height":"100%"
+    }
+    return(
+      <div style={style} onClick={handleClick} onMouseMove={handleMove}>
+        <p>{this.state.mx},{this.state.my}</p>
+        {this.state.marks.map((value)=>{
+          const styleMark = {
+            position:"absolute",
+            top:value.y,
+            left:value.x,
+            width:"10px",
+            display:(value.isHide)?"none":"block",
+            transform:`translate(${value.offsetX}px,${value.offsetY}px)`
+          }
+          return <img key={value.id} src={imgCircle} style={styleMark}/>
+
+
+        })}
+
+      </div>
+    )
+  }
+
+  render(){
     const style = {
       "position":"absolute",
       "width":"500px",
       "top":"100px",
       "left":"50px",
-      "border-style":"solid"
+      "borderStyle":"solid",
+      "overflow":"hidden"
     }
     return (
-    <div id="bodyMag" style={style} onClick={handleClick}>
+    <div id="bodyMag" style={style}>
 
       <GlassMagnifier
         imageSrc={image2}
         imageAlt="Example"
-        renderOverlay={ovl}
+        renderOverlay={this.ovl}
         cursorStyle="crosshair"
+        onImageLoad={(e)=>{this.setState({imgSize:e.target.naturalWidth})}}
       />
     </div>
     )
